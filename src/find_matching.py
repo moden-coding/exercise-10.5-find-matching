20a2f1575b2c66736dcb481ee4fff1b53dd55ed5
modengann <99417989+modengann@users.noreply.github.com>
2022-08-29 11:49:13 -0400

    Update find_matching.py

--- src/find_matching.py
+++ src/find_matching.py
@@ -1,16 +1,9 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    temp = []
-    for i, x in enumerate(L):
-        if pattern in x:
-            temp.append(i)
-    
-    return temp
-
+    pass
 def main():
-     list = ["sensitive", "engine", "rubbish", "comment"]
-     print(find_matching(list,'en'))
+    pass
 
 if __name__ == "__main__":
     main()
